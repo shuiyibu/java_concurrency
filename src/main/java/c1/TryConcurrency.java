a34ca7e5fb728e66ef7e2ab34ccedd83e68d138f
shuiyibu <shuiyibu.ja@gmail.com>
2019-08-31 13:56:22 +0800

    Initial commit

---
+++ src/main/java/c1/TryConcurrency.java
@@ -0,0 +1,13 @@
+package c1;
+
+/**
+ * @Auther: langdylan
+ * @Date: 2019-08-31 13:54
+ * @Description:
+ */
+public class TryConcurrency {
+    public static void main(String[] args) {
+
+    }
+
+}
